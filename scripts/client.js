6e92d22f97512cd88c89a2e9487c2be7453b345e
Maggie Dou <maggie.d0228@hotmail.com>
2021-03-28 23:26:27 -0700

    initial commit

---
+++ scripts/client.js
@@ -0,0 +1,15 @@
+$(function () {
+
+  $(window). on ("load", function () {
+    $(".responsiveNav").css({"display":"none"});
+  })
+
+  $("#hamMenu img").on("click", function() {
+    if ($(".responsiveNav").css("display") == "none") {
+      $(".responsiveNav").css({"display":"block"});
+    } else {
+      $(".responsiveNav").css({"display":"none"});
+    }
+  })
+
+});
